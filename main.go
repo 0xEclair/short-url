a73719a9466f5d44763f306babbfc290545b4d9f
eclair-lumiere <38656355+eclair-lumiere@users.noreply.github.com>
2022-05-12 19:54:24 +0800

    :sparkles: set up dev environment

--- main.go
+++ main.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"fmt"
+	
+	"github.com/gin-gonic/gin"
+)
+
 func main() {
-	println("hello, short url")
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Hey , to shorten url",
+		})
+	})
+	
+	err := r.Run(":9808")
+	if err != nil {
+		panic(fmt.Sprintf("failed to start the server - Error: %v", err))
+	}
 }
