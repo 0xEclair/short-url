4a559c03ba1b31577424c8564dddf0a1d6828ee5
eclair-lumiere <38656355+eclair-lumiere@users.noreply.github.com>
2022-05-15 00:25:26 +0800

    :white_check_mark: redis test

---
+++ cache/redis_test.go
@@ -0,0 +1,27 @@
+package cache
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+var testStoreService = &StorageService{}
+
+func init() {
+	testStoreService = InitializeStore()
+}
+
+func TestStoreInit(t *testing.T) {
+	assert.True(t, testStoreService.redisClient != nil)
+}
+
+func TestInsertionAndRetrieval(t *testing.T) {
+	initialLink := "https://www.bilibili.com/"
+	userUUID := "e0dba740-fc4b-4977-872c-d360239e6b1a"
+	shortURL := "D3vgAs5oK"
+	
+	SaveUrlMapping(shortURL, initialLink, userUUID)
+	retrievedURL := RetrieveInitialUrl(shortURL)
+	
+	assert.Equal(t, initialLink, retrievedURL)
+}
